dcc76cdfa9957498587ef4b1cf616431adc30b13
Charlotte Manser <k2582972@kcl.ac.uk>
2026-01-26 16:38:27 +0000

    switching over branch

--- helpers/runSimViscoManser.m
+++ helpers/runSimViscoManser.m
@@ -7,37 +7,24 @@ function output = runSimViscoManser(params, progressFlag)
     %% Initial discretisation
     RsMinusB = zeros(params.nT, params.nR);
     RsMinusB(1,:) = linspace(0, params.B, params.nR) - params.B;
-
     ts = linspace(0, params.tFinal, params.nT);
 
-    %% Computed values to store
+    %% Preallocate
     rs = zeros(params.nT, params.nR);
-
     drdts = zeros(params.nT, params.nR);
     drdts(1,:) = 0;
-
-    growthStretches = zeros(params.nT, params.nR);
-    growthStretches(1,:) = 1;
-
+    growthStretches = ones(params.nT, params.nR);
     dgrowthStretchesdt = zeros(params.nT, params.nR);
-    dgrowthStretchesdt(1,:) = 0;
-
     growthRates = zeros(params.nT, params.nR);
     radialStresses = zeros(params.nT, params.nR);
     hoopStresses = zeros(params.nT, params.nR);
     bulkStresses = zeros(params.nT, params.nR);
     viscoelasticStretches = zeros(params.nT, params.nR);
     nutrients = zeros(params.nT, params.nR);
-
-    necroticRadii = zeros(params.nT, 1) - Inf;
-
-    densities = zeros(params.nT, params.nR);  % NEW
-
-    mus = zeros(params.nT, params.nR);
-    mus(1,:) = params.mu0;
-
-    varsigmas = zeros(params.nT, params.nR);
-    varsigmas(1,:) = params.varsigma0;
+    necroticRadii = -Inf * ones(params.nT,1);
+    densities = zeros(params.nT, params.nR);
+    mus = zeros(params.nT, params.nR); mus(1,:) = params.mu0;
+    varsigmas = zeros(params.nT, params.nR); varsigmas(1,:) = params.varsigma0;
 
     %% Progress bar
     if progressFlag
@@ -53,131 +40,87 @@ function output = runSimViscoManser(params, progressFlag)
         end
 
         %% Eulerian radial coordinates
-        rs(tInd,:) = computeRadialCoord( ...
-            RsMinusB(tInd,:), growthStretches(tInd,:), params);
+        rs(tInd,:) = computeRadialCoord(RsMinusB(tInd,:), growthStretches(tInd,:), params);
 
         %% Update material parameters
         switch params.materialmodel
             case 'static'
                 [mu_now, varsigma_now] = staticMaterialProperties(params);
-                mus(tInd,:) = mu_now * ones(1, params.nR);
-                varsigmas(tInd,:) = varsigma_now * ones(1, params.nR);
-
+                mus(tInd,:) = mu_now; varsigmas(tInd,:) = varsigma_now;
             case 'autonomous'
-                [mu_now, varsigma_now] = ...
-                    autonomousMaterialProperties(ts(tInd), params);
-                mus(tInd,:) = mu_now * ones(1, params.nR);
-                varsigmas(tInd,:) = varsigma_now * ones(1, params.nR);
-
+                [mu_now, varsigma_now] = autonomousMaterialProperties(ts(tInd), params);
+                mus(tInd,:) = mu_now; varsigmas(tInd,:) = varsigma_now;
             case 'dynamic'
-                [mus(tInd,:), varsigmas(tInd,:)] = ...
-                    dynamicMaterialProperties(densities(tInd), params);
+                [mus(tInd,:), varsigmas(tInd,:)] = dynamicMaterialProperties(densities(tInd), params);
         end
 
-        %% Stresses
+        %% Compute stresses (with relaxation)
+        if tInd == 1
+            radialStressPrev = [];
+            dt = 0;
+        else
+            radialStressPrev = radialStresses(tInd-1,:);
+            dt = ts(tInd) - ts(tInd-1);
+        end
+
         [radialStresses(tInd,:), ...
          hoopStresses(tInd,:), ...
          bulkStresses(tInd,:), ...
          viscoelasticStretches(tInd,:)] = ...
             computeStressesViscoManser( ...
-                RsMinusB(tInd,:), ...
-                rs(tInd,:), ...
-                drdts(tInd,:), ...
-                growthStretches(tInd,:), ...
-                dgrowthStretchesdt(tInd,:), ...
-                mus(tInd,:), ...
-                varsigmas(tInd,:), ...
-                params);
-
+                RsMinusB(tInd,:), rs(tInd,:), drdts(tInd,:), ...
+                growthStretches(tInd,:), dgrowthStretchesdt(tInd,:), ...
+                mus(tInd,:), varsigmas(tInd,:), ...
+                params, dt, radialStressPrev);
+        
         %% Nutrient
         nutrients(tInd,:) = computeNutrient(rs(tInd,:), params);
 
         %% Necrotic core
-        necroticRadii(tInd) = computeNecroticRadius( ...
-            necroticRadii(1:tInd-1), ...
-            rs(tInd,:), ...
-            nutrients(tInd,:), ...
-            params);
+        necroticRadii(tInd) = computeNecroticRadius(necroticRadii(1:tInd-1), ...
+                                                    rs(tInd,:), nutrients(tInd,:), params);
 
         %% Growth rate
         switch params.model
             case 'A'
-                growthRates(tInd,:) = ...
-                    modelAGrowthRate(nutrients(tInd,:), params);
-
+                growthRates(tInd,:) = modelAGrowthRate(nutrients(tInd,:), params);
             case 'B'
-                growthRates(tInd,:) = ...
-                    modelBGrowthRate(nutrients(tInd,:), rs(tInd,:), ...
-                                     necroticRadii(tInd), ...
-                                     radialStresses(tInd,:), params);
-
+                growthRates(tInd,:) = modelBGrowthRate(nutrients(tInd,:), rs(tInd,:), ...
+                                                      necroticRadii(tInd), radialStresses(tInd,:), params);
             case 'B2'
-                growthRates(tInd,:) = ...
-                    modelB2GrowthRate(nutrients(tInd,:), rs(tInd,:), ...
-                                      necroticRadii(tInd), ...
-                                      radialStresses(tInd,:), params);
-
+                growthRates(tInd,:) = modelB2GrowthRate(nutrients(tInd,:), rs(tInd,:), ...
+                                                       necroticRadii(tInd), radialStresses(tInd,:), params);
             case 'C'
-                growthRates(tInd,:) = ...
-                    modelCGrowthRate(nutrients(tInd,:), rs(tInd,:), ...
-                                     necroticRadii(tInd), ...
-                                     radialStresses(tInd,:), params);
-
+                growthRates(tInd,:) = modelCGrowthRate(nutrients(tInd,:), rs(tInd,:), ...
+                                                      necroticRadii(tInd), radialStresses(tInd,:), params);
             case 'D'
-                growthRates(tInd,:) = ...
-                    modelDGrowthRate(nutrients(tInd,:), rs(tInd,:), ...
-                                     necroticRadii(tInd), ...
-                                     radialStresses(tInd,:), ...
-                                     hoopStresses(tInd,:), params);
-
+                growthRates(tInd,:) = modelDGrowthRate(nutrients(tInd,:), rs(tInd,:), ...
+                                                      necroticRadii(tInd), radialStresses(tInd,:), ...
+                                                      hoopStresses(tInd,:), params);
             case 'E'
-                growthRates(tInd,:) = ...
-                    modelEGrowthRate(nutrients(tInd,:), rs(tInd,:), ...
-                                     necroticRadii(tInd), ...
-                                     radialStresses(tInd,:), ...
-                                     hoopStresses(tInd,:), params);
+                growthRates(tInd,:) = modelEGrowthRate(nutrients(tInd,:), rs(tInd,:), ...
+                                                      necroticRadii(tInd), radialStresses(tInd,:), ...
+                                                      hoopStresses(tInd,:), params);
         end
 
-        %% Density (diagnostic only)
+        %% Density
         densities(tInd,:) = 1 ./ (growthStretches(tInd,:).^3);
 
         %% Advance to next time step
         if tInd < params.nT
-
-            RsMinusB(tInd+1,:) = interp1( ...
-                rs(tInd,:), RsMinusB(tInd,:), ...
-                linspace(rs(tInd,1), rs(tInd,end), params.nR));
-
-            rRemapped = interp1( ...
-                RsMinusB(tInd,:), rs(tInd,:), RsMinusB(tInd+1,:));
-
-            growthStretchRemapped = interp1( ...
-                RsMinusB(tInd,:), growthStretches(tInd,:), ...
-                RsMinusB(tInd+1,:));
-
-            growthRateRemapped = interp1( ...
-                RsMinusB(tInd,:), growthRates(tInd,:), ...
-                RsMinusB(tInd+1,:));
-
-            drdtRemapped = interp1( ...
-                RsMinusB(tInd,:), drdts(tInd,:), ...
-                RsMinusB(tInd+1,:));
-
-            dgrowthStretchesdtRemapped = interp1( ...
-                RsMinusB(tInd,:), dgrowthStretchesdt(tInd,:), ...
-                RsMinusB(tInd+1,:));
+            RsMinusB(tInd+1,:) = interp1(rs(tInd,:), RsMinusB(tInd,:), linspace(rs(tInd,1), rs(tInd,end), params.nR));
+            rRemapped = interp1(RsMinusB(tInd,:), rs(tInd,:), RsMinusB(tInd+1,:));
+            growthStretchRemapped = interp1(RsMinusB(tInd,:), growthStretches(tInd,:), RsMinusB(tInd+1,:));
+            growthRateRemapped = interp1(RsMinusB(tInd,:), growthRates(tInd,:), RsMinusB(tInd+1,:));
+            drdtRemapped = interp1(RsMinusB(tInd,:), drdts(tInd,:), RsMinusB(tInd+1,:));
+            dgrowthStretchesdtRemapped = interp1(RsMinusB(tInd,:), dgrowthStretchesdt(tInd,:), RsMinusB(tInd+1,:));
 
             drdts(tInd+1,:) = drdtRemapped;
             dgrowthStretchesdt(tInd+1,:) = dgrowthStretchesdtRemapped;
 
-            dt = ts(tInd+1) - ts(tInd);
-
-            growthStretches(tInd+1,:) = ...
-                growthStretchRemapped + ...
-                dt * growthStretchRemapped .* growthRateRemapped;
-
-            dgrowthStretchesdt(tInd+1,:) = ...
-                (growthStretches(tInd+1,:) - growthStretches(tInd,:)) / dt;
+            dtLocal = ts(tInd+1) - ts(tInd);
+            growthStretches(tInd+1,:) = growthStretchRemapped + dtLocal * growthStretchRemapped .* growthRateRemapped;
+            dgrowthStretchesdt(tInd+1,:) = (growthStretches(tInd+1,:) - growthStretches(tInd,:)) / dtLocal;
         end
     end
 
@@ -185,7 +128,7 @@ function output = runSimViscoManser(params, progressFlag)
         textprogressbar('\nDone.');
     end
 
-    %% Output
+    %% Pack outputs
     output = struct();
     output.ts = ts;
     output.RsMinusB = RsMinusB;
--- runPair.m
+++ runPair.m
@@ -1,88 +1,58 @@
 addpath(genpath('./helpers'))
 
-%% Specify model parameters and setup. All quantities are in SI units.
+%% Specify model parameters
 params = struct();
-
-% Model identifier, one of 'A', 'B', 'C', 'D', or 'E', corresponding to the
-% accompanying publication.
 params.model = 'B';
-
-% Time and discretisation.
-params.tFinal = 15; % 20; % Final time.
-params.nR = 3000; % Number of points in the spatial discretisation.
-params.nT = 4000; % Number of points in the temporal discretisation.
-
-% Material parameters.
-params.kappa = 100; %316.2; % Spring constant in the radial stress boundary condition.
-
-% Growth rate parameters.
-params.k = 1; % The basic growth rate constant.
-params.sigmaHat = -100; % Threshold below which growth is arrested due to compressive stress, if included in the model.
-params.beta = 1; % Scale factor for the local argument of n, if included in the model.
-
-% Nutrient parameters.
-params.cInf = 5; % Concentration of nutrient at the boundary.
-params.cHat = 4; % Nutrient threshold for necrosis.
-params.lambda = 0.9; % Rate of nutrient consumption by tissue.
-params.D = 1; % Diffusion coefficient of nutrient inside the tumour.
-params.L = sqrt(params.D * params.cInf/params.lambda); % The diffusive lengthscale.
-params.bHat = sqrt(6)*params.L; % The threshold radius of the tumour for full/partial perfusion.
-
-% Initial configuration.
-params.B = params.L; % Initial (and unstressed) radius of the spheroid.
-
-% Timescale
-params.T = 1 / (params.k * params.cInf);
-% Constant thresholds for Taylor expanding integrals.
-params.radialStressIntegrandThreshold = 0.05; % Radial threshold for using Taylor expansion of radial stress integrand.
-params.elasticStretchIntegrandThreshold = 0.05; % Radial threshold for using Taylor expansion of of elastic stretch integrand.
+params.tFinal = 30;
+params.nR = 3000;
+params.nT = 5000;
+params.kappa = 100;
+params.k = 1;
+params.sigmaHat = -100;
+params.beta = 1;
+params.cInf = 5;
+params.cHat = 4;
+params.lambda = 0.9;
+params.D = 1;
+params.L = sqrt(params.D * params.cInf / params.lambda);
+params.B = params.L;
+params.bHat = sqrt(6) * params.L; % threshold radius for nutrient perfusion
+params.T = 1 / (params.k * params.cInf); % timescale
+params.radialStressIntegrandThreshold = 0.05;
+params.elasticStretchIntegrandThreshold = 0.05;
 
 
+
+%% Copy params for elastic and viscoelastic runs
 params1 = params;
 params2 = params;
 
-%% Pick run specific parameters
-
+%% Elastic case
 params1.label = 'elastic';
-params1.mu0 = 0.1;
-params1.varsigma0 = 0;
-%params1.jammingeffect = 0;
+params1.mu0 = 1;
+params1.varsigma0 = 5;
 params1.materialmodel = 'static';
-params1.model = 'B';
+params1.tau = 1000; % relaxation timescale
 
-
+%% Viscoelastic case
 params2.label = 'viscoelastic';
-params2.mu0 = 0.1;
+params2.mu0 = 1;
 params2.varsigma0 = 5;
-%params2.jammingeffect = 0;
 params2.materialmodel = 'static';
-params2.model = 'B';
+params2.tau = 0.01; % relaxation timescale
 
-
-%% Run the simulation.
+%% Run simulations
 output1 = runSimViscoManser(params1);
 output2 = runSimViscoManser(params2);
 
-%% Plotting.
-%plot_spheroid_pair(output1, output2, 'Elastic', 'Viscoelastic', 1);
-
+%% Plot results
 plot_spheroid_pair(output1, output2, params1.label, params2.label, params.nT, false);
-plot_boundary_stress_pair(output1, output2, params1.label, params2.label)
-%plot_evolution_pair(output1, output2,params1.label, params2.label);
-elastic_and_growth_stretches(output1, output2,params1.label, params2.label);
-%plot_boundary_stress_pair(output1, output2, params1.label, params2.label);
-%plot_boundary_stress_combined_pair(output1, output2, params1.label, params2.label)
-%plot_material_pair(output1, output2, params1.label, params2.label);
+plot_boundary_stress_pair(output1, output2, params1.label, params2.label);
+%elastic_and_growth_stretches(output1, output2, params1.label, params2.label);
+plot_evolution_pair(output1, output2, params1.label, params2.label);
 
-
-%plot_spheroid(output, params.nT);
-%plot_evolution(output);
-%make_spheroid_video(output,10,10,'spheroid_growth_E_high_res.mp4');  
-
-%% Saving
+%% Save reduced output
 output = output2;
-
-% Generate reduced output.
 reducedOutput = struct();
 reducedOutput.ts = output.ts;
 reducedOutput.radii = output.rs(:,end);
@@ -97,5 +67,4 @@ reducedOutput.hoopStressFinal = output.hoopStresses(end,:);
 reducedOutput.params = params;
 reducedOutput.necroticRadii = output.necroticRadii;
 
-% save('output.mat','output')
-save('reducedOutputVisco.mat','reducedOutput')
+save('reducedOutputVisco.mat','reducedOutput');
